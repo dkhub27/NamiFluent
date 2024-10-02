111cdfa3b26fa09df357a62bc2e1e4842c860080
dkhub27 <christianmalvao2023@gmail.com>
2024-10-02 00:22:45 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,4 +3,10 @@ local Fluent = {}
 
 -- Exemplo de como você pode requerer os arquivos Lua dentro de suas pastas
 Fluent.Creator = loadstring(game:HttpGet("https://raw.githubusercontent.com/dkhub27/NamiFluent/refs/heads/main/Creator.lua"))()
+-- Carregando os temas
+Fluent.Themes = loadstring(game:HttpGet("https://raw.githubusercontent.com/dkhub27/NamiFluent/refs/heads/main/Themes/init.lua"))()
 
+-- Você pode adicionar mais arquivos da mesma forma
+
+-- Retorna a library completa para o Roblox
+return Fluent
